fdbc3d2978a0b51d2b51c47157be4719f4829d1c
Oxana Theis <otheis@protonmail.com>
2023-10-04 12:19:32 +0100

    modify reset form

--- src/components/form-reset-password/index.js
+++ src/components/form-reset-password/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { authData } from "../../services/auth-services";
+import Popup from "../popup";
 import { cn as bem } from "@bem-react/classname";
 import "./style.css";
 
@@ -9,24 +10,27 @@ const FormResetPassword = () => {
   const [user, setUser] = useState({
     password: "",
   });
-  
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [errorMessage, setErrorMessage] = useState({});
   const location = useLocation();
 
   function handleSubmit(e) {
+    
+    setUser({ password: "" }) 
     e.preventDefault();
     let formdata = new FormData();
-    formdata.append("password", `${user.password}`);
-    const apiEndpoint = location.pathname;
-
+    formdata.append("password", user.password);
+    // const apiEndpoint = location.pathname;
+    const apiEndpoint = `${location.pathname+location.search}`;
+console.log(apiEndpoint)
     async function submitForm() {
       try {
         const result = await authData(apiEndpoint, formdata);
         setErrorMessage(result.message);
 
-        // if (result.message.error === undefined && user.email !== "") {
-        //   setShowSuccessMessage(true);
-        // }
+        if (result.message.password === undefined && user.password!== "") {
+          setShowSuccessMessage(true);
+        }
       } catch (error) {
         // Handle error case
         console.log(error);
@@ -34,27 +38,69 @@ const FormResetPassword = () => {
     }
     submitForm();
   }
-
+console.log(errorMessage)
 
   return (
-    <form className={cn()}  onSubmit={(e) => {
-      handleSubmit(e);
-    }}>
-      <div className={cn()}>
-        <h2>Reset password</h2>
 
-        <div className={cn("input")}>
-          <label>Enter your new password </label>
-          <input type="email" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })}></input>
+    <>
+    {!showSuccessMessage ? (
+      <form className={cn()} onSubmit={handleSubmit}>
+        <div className={cn()}>
+          <h2>Reset password</h2>
+          <div className={cn("input")}>
+            <label>Enter your new password </label>
+            <input
+              type="password"
+              name="password"
+              value={user.password}
+              onChange={(e) => setUser({ password: e.target.value })}
+            />
+          </div>
+          {errorMessage?.password ? (
+            <p className={cn("error-msg")}>{errorMessage.password}</p>
+          ) : null}
+          {/* <div className={cn("input")}>
+            <label>Confirm new password </label>
+            <input type="password" name="confirmPassword" value={user.confirmPassword} />
+          </div> */}
+          <button type="submit">Reset password</button>
         </div>
+      </form>
+    ) : (
+      <Popup text={"Password modified"} link={"/login"} />
+    )}
+  </>
+  //  {!showSuccessMessage ?
+  //   (
+  //   <form
+  //   className={cn()}
+  //   onSubmit={(e) => {
+  //     handleSubmit(e);
+  //   }}
+  // >
+  //   <div className={cn()}>
+  //     <h2>Reset password</h2>
 
-        <div className={cn("input")}>
-          <label>Confirm new password </label>
-          <input type="email"></input>
-        </div>
-        <button type="submit">Reset password</button>
-      </div>
-    </form>
+  //     <div className={cn("input")}>
+  //       <label>Enter your new password </label>
+  //       <input type="password" name="password" value={user.password}  onChange={(e) => setUser({ password: e.target.value })}></input>
+  //     </div>
+  //     {errorMessage?.password? <p className={cn("error-msg")}>{errorMessage.password}</p> : ""}
+  //     {/* <div className={cn("input")}>
+  //       <label>Confirm new password </label>
+  //       <input type="password" name="password" value={user.password} ></input>
+  //     </div> */}
+  //     <button type="submit">Reset password</button>
+  //   </div>
+  // </form>
+  
+  //   ):(
+  //     <Popup text={"Password reset email sent successfully to your email"} link={"/login"}/>
+  //   )
+  
+ 
+
+  // }
   );
 };
 
