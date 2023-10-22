43376a32bd3bf389480423836e53cb65c35e9d09
Oxana Theis <otheis@protonmail.com>
2023-10-22 16:24:56 +0100

    add employees aside

--- src/containers/employees/index.js
+++ src/containers/employees/index.js
@@ -5,13 +5,16 @@ import Employee from "../../components/employee";
 import Heading from "../../components/heading";
 import woman from "../../assets/woman.png";
 import phone from "../../assets/phone.png";
-
+import man from "../../assets/man.png"
+import boy from "../../assets/boy.png"
 const Employees = () => {
   const cn = bem("Employees");
   return (
     <div className={cn()}>
       <Heading title={"New employees"} />
       <Employee photo={woman} title={`Melanie\nSantos`} role="Designer" icon={phone} phone={"3599"} />
+      <Employee photo={man} title={`Nico\nReis`} role="IT Engeener" icon={phone} phone={"3589"} />
+      {/* <Employee photo={boy} title={`Adam\nColin`} role="Developer" icon={phone} phone={"3586"} /> */}
     </div>
   );
 };
